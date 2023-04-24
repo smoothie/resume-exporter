106a283e3fa6603801e4f50c529c43484084a834
Marc Eichenseher <this@marceichenseher.de>
2023-04-24 07:02:30 +0200

    feat(qa): Convert double quotes to single
    feat(qa): Enable Symfony ruleset (seemed they were are omitted somehow)

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,8 +1,8 @@
 <?php
 
 $workingDirectory = __DIR__;
-$srcDirectory = $workingDirectory . '/src';
-$unitTestDirectory = $workingDirectory . '/tests/Unit';
+$srcDirectory = $workingDirectory.'/src';
+$unitTestDirectory = $workingDirectory.'/tests/Unit';
 
 if (is_readable($srcDirectory) === false) {
     throw new RuntimeException('Unable to find ./src directory. What did you do??!');
@@ -21,11 +21,14 @@ $config = new PhpCsFixer\Config();
 return $config->setRules([
     '@PhpCsFixer:risky' => true,
     // [PER:risky & Symfony:risky source: https://cs.symfony.com/doc/ruleSets/PhpCsFixerRisky.html]
+    '@Symfony' => true,
     '@PHP82Migration' => true,
     'trailing_comma_in_multiline' => ['elements' => ['arguments', 'arrays', 'match', 'parameters']],
     'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     'phpdoc_to_param_type' => true,
     'date_time_immutable' => true,
+    'single_quote' => ['strings_containing_single_quote_chars' => true],
+    'cast_spaces' => ['space' => 'single'],
     'blank_line_before_statement' => ['statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try']],
     'no_extra_blank_lines' => [
         'tokens' => [
